d65060f349ee5b037aabd2dd04d2b6fb16d0125f
snowWolf350 <pranet.v08@gmail.com>
2026-08-09 20:37:25 +0530

    added cards

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// =========================================
+// PROJECT CAROUSEL
+// =========================================
 
+function scrollCarousel(id, direction) {
+
+    // Find the carousel
+    const carousel = document.getElementById(id);
+
+    // Get the width of one project
+    const amount = carousel.clientWidth;
+
+    // Move one project left or right
+    carousel.scrollBy({
+        left: amount * direction,
+        behavior: "smooth"
+    });
+
+}
